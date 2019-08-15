597816fa482dd94234b5ffe9b8a1b3cd628701ca
Jason Lee <jason@zerodevx.com>
2019-08-15 23:01:39 +0800

    Add tests

---
+++ test/index.test.js
@@ -0,0 +1,72 @@
+const {expect, test} = require('@oclif/test');
+const cmd = require('../src');
+
+describe('#index', () => {
+
+  test
+    .stdout()
+    .do(() => cmd.run(['https://example.com', '--root', 'test/test-site/about']))
+    .it('basic sitemap xml', ctx => {
+      expect(ctx.stdout).to.contain('<loc>https://example.com</loc>');
+      expect(ctx.stdout).to.contain('<?xml version="1.0" encoding="UTF-8"?>');
+      expect(ctx.stdout).to.contain('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">');
+    });
+
+  test
+    .stdout()
+    .do(() => cmd.run(['https://example.com', '--root', 'test/test-site/about', '--text']))
+    .it('output text', ctx => {
+      expect(ctx.stdout).to.equal('https://example.com\n\n');
+    });
+
+  test
+    .stdout()
+    .do(() => cmd.run(['https://example.com', '--root', 'test/test-site', '--priority', 'about/index.html=0.1']))
+    .it('priority', ctx => {
+      expect(ctx.stdout).to.contain('<priority>0.1</priority>');
+    });
+
+  test
+    .stdout()
+    .do(() => cmd.run(['https://example.com', '--root', 'test/test-site', '--changefreq', 'about/index.html=weekly']))
+    .it('changefreq', ctx => {
+      expect(ctx.stdout).to.contain('<changefreq>weekly</changefreq>');
+    });
+
+  test
+    .stdout()
+    .do(() => cmd.run(['https://example.com', '--root', 'test/test-site']))
+    .it('clean urls', ctx => {
+      expect(ctx.stdout).to.not.contain('.html');
+      expect(ctx.stdout).to.contain('post-1</loc>');
+      expect(ctx.stdout).to.contain('https://example.com/blog/events/event-1</loc>');
+    });
+
+  test
+    .stdout()
+    .do(() => cmd.run(['https://example.com', '--root', 'test/test-site', '--no-clean']))
+    .it('no clean urls', ctx => {
+      expect(ctx.stdout).to.contain('https://example.com/index.html</loc>');
+      expect(ctx.stdout).to.contain('https://example.com/blog/news/post-2.html</loc>');
+    });
+
+  test
+    .stdout()
+    .do(() => cmd.run(['https://example.com', '--root', 'test/test-site', '--slash']))
+    .it('trailing slash', ctx => {
+      expect(ctx.stdout).to.contain('https://example.com/</loc>');
+      expect(ctx.stdout).to.contain('https://example.com/blog/mixed-1/</loc>');
+    });
+
+
+  /*
+  test
+    .stdout()
+    .do(() => cmd.run([]))
+    .it('the test', ctx => {
+      expect(ctx.stdout)
+    });
+  */
+
+});
+
