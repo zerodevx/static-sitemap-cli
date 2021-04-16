05454371bea45eef72c3f4f4c21d1960ca6382e5
Jason Lee <jason@zerodevx.com>
2021-04-16 14:28:55 +0800

    Use Standardjs instead

--- src/index.js
+++ src/index.js
@@ -1,107 +1,107 @@
-const { Command, flags } = require('@oclif/command');
-const getStdin = require('get-stdin');
-const fg = require('fast-glob');
-const mm = require('micromatch');
-const parser = require('js2xmlparser');
-const fs = require('fs');
-const htmlparser = require('htmlparser2');
+const { Command, flags } = require('@oclif/command')
+const getStdin = require('get-stdin')
+const fg = require('fast-glob')
+const mm = require('micromatch')
+const parser = require('js2xmlparser')
+const fs = require('fs')
+const htmlparser = require('htmlparser2')
 
 class StaticSitemapCliCommand extends Command {
-  async run() {
-    const { argv, flags } = this.parse(StaticSitemapCliCommand);
+  async run () {
+    const { argv, flags } = this.parse(StaticSitemapCliCommand)
 
-    let baseUrl = await getStdin();
+    let baseUrl = await getStdin()
     if (!baseUrl) {
       if (!argv.length) {
         this.error('you must include a BASEURL - type "sscli --help" for help.', {
           code: 'BASEURL_NOT_FOUND',
-          exit: 1,
-        });
+          exit: 1
+        })
       }
-      baseUrl = argv[0];
+      baseUrl = argv[0]
     }
 
-    const addSlash = (path) => (path.slice(-1) === '/' ? path : `${path}/`);
-    baseUrl = addSlash(baseUrl);
+    const addSlash = (path) => (path.slice(-1) === '/' ? path : `${path}/`)
+    baseUrl = addSlash(baseUrl)
 
     const getUrl = (path) => {
-      let url = baseUrl + path;
+      let url = baseUrl + path
       if (!flags['no-clean']) {
         if (url.slice(-11) === '/index.html') {
-          url = url.slice(0, -11);
+          url = url.slice(0, -11)
         } else if (url.slice(-5) === '.html') {
-          url = url.slice(0, -5);
+          url = url.slice(0, -5)
         }
       }
       if (flags.slash || url.split('/').length === 3) {
-        url = url + '/';
+        url = url + '/'
       }
-      return url;
-    };
+      return url
+    }
 
     const files = await fg(flags.match, {
       cwd: flags.root,
-      stats: true,
-    });
+      stats: true
+    })
     if (files.length === 0) {
       this.error('[static-sitemap-cli] no file matches found!', {
         code: 'NO_MATCHES_FOUND',
-        exit: 1,
-      });
+        exit: 1
+      })
     }
     if (flags.verbose) {
-      console.warn('\x1b[36m%s\x1b[0m', `[static-sitemap-cli] found ${files.length} files!`);
+      console.warn('\x1b[36m%s\x1b[0m', `[static-sitemap-cli] found ${files.length} files!`)
       for (let a = 0; a < files.length - 1; a++) {
-        console.warn('\x1b[36m%s\x1b[0m', `[static-sitemap-cli] -${files[a].path}`);
+        console.warn('\x1b[36m%s\x1b[0m', `[static-sitemap-cli] -${files[a].path}`)
       }
     }
 
-    let sitemapText = '';
+    let sitemapText = ''
     for (let a = 0; a < files.length - 1; a++) {
-      sitemapText += getUrl(files[a].path) + '\n';
+      sitemapText += getUrl(files[a].path) + '\n'
     }
-    sitemapText += getUrl(files[files.length - 1].path);
+    sitemapText += getUrl(files[files.length - 1].path)
 
     if (flags.text) {
-      this.log(sitemapText);
-      return;
+      this.log(sitemapText)
+      return
     }
 
-    let urls = [];
+    const urls = []
     for (let a = 0; a < files.length; a++) {
-      let obj = {
+      const obj = {
         loc: getUrl(files[a].path),
-        lastmod: files[a].stats.mtime.toISOString(),
-      };
+        lastmod: files[a].stats.mtime.toISOString()
+      }
 
       if (flags['follow-noindex']) {
-        const fileContent = fs.readFileSync(flags.root + '/' + files[a].path);
+        const fileContent = fs.readFileSync(flags.root + '/' + files[a].path)
 
-        let noindex = false;
+        let noindex = false
 
         const parsedHtml = new htmlparser.Parser({
-          onopentag(name, attrs) {
+          onopentag (name, attrs) {
             if (name === 'meta' && attrs.name === 'robots' && attrs.content === 'noindex') {
-              noindex = true;
-              parsedHtml.end();
+              noindex = true
+              parsedHtml.end()
             }
-          },
-        });
+          }
+        })
 
-        parsedHtml.write(fileContent);
-        parsedHtml.end();
+        parsedHtml.write(fileContent)
+        parsedHtml.end()
 
         // No index meta tag
 
         if (noindex) {
-          continue;
+          continue
         }
       }
 
       if (flags.priority) {
         for (let b = 0; b < flags.priority.length; b++) {
           if (mm.isMatch(files[a].path, flags.priority[b].split('=')[0])) {
-            obj.priority = parseFloat(flags.priority[b].split('=')[1]);
+            obj.priority = parseFloat(flags.priority[b].split('=')[1])
           }
         }
       }
@@ -109,37 +109,37 @@ class StaticSitemapCliCommand extends Command {
       if (flags.changefreq) {
         for (let b = 0; b < flags.changefreq.length; b++) {
           if (mm.isMatch(files[a].path, flags.changefreq[b].split('=')[0])) {
-            obj.changefreq = flags.changefreq[b].split('=')[1];
+            obj.changefreq = flags.changefreq[b].split('=')[1]
           }
         }
       }
-      urls.push(obj);
+      urls.push(obj)
     }
 
-    let sitemap = parser.parse(
+    const sitemap = parser.parse(
       'urlset',
       {
         '@': {
-          xmlns: 'http://www.sitemaps.org/schemas/sitemap/0.9',
+          xmlns: 'http://www.sitemaps.org/schemas/sitemap/0.9'
         },
-        url: [urls],
+        url: [urls]
       },
       {
         declaration: {
-          encoding: 'UTF-8',
+          encoding: 'UTF-8'
         },
         format: {
-          doubleQuotes: true,
-        },
-      },
-    );
+          doubleQuotes: true
+        }
+      }
+    )
 
     if (flags.save) {
-      let outputDir = flags['output-dir'] || flags.root;
-      fs.writeFileSync(`${addSlash(outputDir)}sitemap.xml`, `${sitemap}\n`, 'utf-8');
-      fs.writeFileSync(`${addSlash(outputDir)}sitemap.txt`, `${sitemapText}\n`, 'utf-8');
+      const outputDir = flags['output-dir'] || flags.root
+      fs.writeFileSync(`${addSlash(outputDir)}sitemap.xml`, `${sitemap}\n`, 'utf-8')
+      fs.writeFileSync(`${addSlash(outputDir)}sitemap.txt`, `${sitemapText}\n`, 'utf-8')
     } else {
-      this.log(sitemap);
+      this.log(sitemap)
     }
   }
 }
@@ -150,15 +150,15 @@ CLI to pre-generate XML sitemaps for static sites locally.
 At its most basic, just run from root of distribution:
 $ sscli https://example.com > sitemap.xml
 
-CLI by default outputs to 'stdout'; BASEURL can be piped in via 'stdin'.`;
+CLI by default outputs to 'stdout'; BASEURL can be piped in via 'stdin'.`
 
 StaticSitemapCliCommand.args = [
   {
     name: 'baseUrl',
     required: false,
-    description: 'Base URL that is prefixed to all location entries.\nFor example: https://example.com/',
-  },
-];
+    description: 'Base URL that is prefixed to all location entries.\nFor example: https://example.com/'
+  }
+]
 
 StaticSitemapCliCommand.flags = {
   version: flags.version({ char: 'V' }),
@@ -166,61 +166,61 @@ StaticSitemapCliCommand.flags = {
   root: flags.string({
     char: 'r',
     description: 'root working directory',
-    default: '.',
+    default: '.'
   }),
   match: flags.string({
     char: 'm',
     multiple: true,
     description: 'globs to match',
-    default: ['**/*.html', '!404.html'],
+    default: ['**/*.html', '!404.html']
   }),
   priority: flags.string({
     char: 'p',
     multiple: true,
-    description: 'glob-priority pair [eg: foo/**=0.1]',
+    description: 'glob-priority pair [eg: foo/**=0.1]'
   }),
   changefreq: flags.string({
     char: 'c',
     multiple: true,
-    description: 'glob-changefreq pair (eg: bar/**=daily)',
+    description: 'glob-changefreq pair (eg: bar/**=daily)'
   }),
   'no-clean': flags.boolean({
     char: 'n',
     description: 'disable clean URLs',
-    default: false,
+    default: false
   }),
   slash: flags.boolean({
     char: 'l',
     description: 'add trailing slash to all URLs',
     default: false,
-    exclusive: ['no-clean'],
+    exclusive: ['no-clean']
   }),
   'follow-noindex': flags.boolean({
     description: 'removes pages with noindex meta tag from sitemap (up to 5x slower due to reading and parsing every HTML file)',
-    default: false,
+    default: false
   }),
   text: flags.boolean({
     char: 't',
     description: 'output as .TXT instead',
     default: false,
-    exclusive: ['priority', 'changefreq'],
+    exclusive: ['priority', 'changefreq']
   }),
   save: flags.boolean({
     char: 's',
     description: 'save output to XML and TXT files directly',
     default: false,
-    exclusive: ['text'],
+    exclusive: ['text']
   }),
   'output-dir': flags.string({
     char: 'o',
     description: 'specify the output dir; used together with --save; defaults to root working directory',
-    dependsOn: ['save'],
+    dependsOn: ['save']
   }),
   verbose: flags.boolean({
     char: 'v',
     description: 'be more verbose',
-    default: false,
-  }),
-};
+    default: false
+  })
+}
 
-module.exports = StaticSitemapCliCommand;
+module.exports = StaticSitemapCliCommand
--- test/index.test.js
+++ test/index.test.js
@@ -1,114 +1,116 @@
-const { expect, test } = require('@oclif/test');
-const cmd = require('../src');
-const fs = require('fs');
+/* global describe */
+
+const { expect, test } = require('@oclif/test')
+const cmd = require('../src')
+const fs = require('fs')
 
 describe('#index', () => {
   test
     .stdout()
     .do(() => cmd.run(['https://example.com', '--root', 'test/test-site']))
     .it('basic sitemap xml', (ctx) => {
-      expect(ctx.stdout).to.contain('<loc>https://example.com/</loc>');
-      expect(ctx.stdout).to.contain('<loc>https://example.com/about</loc>');
-      expect(ctx.stdout).to.contain('<loc>https://example.com/blog</loc>');
-      expect(ctx.stdout).to.contain('<loc>https://example.com/blog/mixed-2</loc>');
-      expect(ctx.stdout).to.contain('<loc>https://example.com/blog/events/event-1</loc>');
-      expect(ctx.stdout).to.contain('<?xml version="1.0" encoding="UTF-8"?>');
-      expect(ctx.stdout).to.contain('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">');
-    });
+      expect(ctx.stdout).to.contain('<loc>https://example.com/</loc>')
+      expect(ctx.stdout).to.contain('<loc>https://example.com/about</loc>')
+      expect(ctx.stdout).to.contain('<loc>https://example.com/blog</loc>')
+      expect(ctx.stdout).to.contain('<loc>https://example.com/blog/mixed-2</loc>')
+      expect(ctx.stdout).to.contain('<loc>https://example.com/blog/events/event-1</loc>')
+      expect(ctx.stdout).to.contain('<?xml version="1.0" encoding="UTF-8"?>')
+      expect(ctx.stdout).to.contain('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">')
+    })
 
   test
     .stdout()
     .do(() => cmd.run(['https://example.com', '--root', 'test/test-site/about', '--text']))
     .it('output text correctly without double newlines', (ctx) => {
-      expect(ctx.stdout).to.equal('https://example.com/\n');
-    });
+      expect(ctx.stdout).to.equal('https://example.com/\n')
+    })
 
   test
     .stdout()
     .do(() => cmd.run(['https://example.com', '--root', 'test/test-site', '--match', 'about/*.html', '--text']))
     .it('matches only what was specified', (ctx) => {
-      expect(ctx.stdout).to.equal('https://example.com/about\n');
-    });
+      expect(ctx.stdout).to.equal('https://example.com/about\n')
+    })
 
   test
     .stdout()
     .do(() => cmd.run(['https://example.com', '--root', 'test/test-site', '--priority', 'about/index.html=0.1']))
     .it('priority', (ctx) => {
-      expect(ctx.stdout).to.contain('<priority>0.1</priority>');
-    });
+      expect(ctx.stdout).to.contain('<priority>0.1</priority>')
+    })
 
   test
     .stdout()
     .do(() => cmd.run(['https://example.com', '--root', 'test/test-site', '--changefreq', 'about/index.html=weekly']))
     .it('changefreq', (ctx) => {
-      expect(ctx.stdout).to.contain('<changefreq>weekly</changefreq>');
-    });
+      expect(ctx.stdout).to.contain('<changefreq>weekly</changefreq>')
+    })
 
   test
     .stdout()
     .do(() => cmd.run(['https://example.com', '--root', 'test/test-site', '-c', 'about/index.html=daily']))
     .it('changefreq', (ctx) => {
-      expect(ctx.stdout).to.contain('<changefreq>daily</changefreq>');
-    });
+      expect(ctx.stdout).to.contain('<changefreq>daily</changefreq>')
+    })
 
   test
     .stdout()
     .do(() => cmd.run(['https://example.com', '--root', 'test/test-site']))
     .it('clean urls', (ctx) => {
-      expect(ctx.stdout).to.not.contain('.html');
-      expect(ctx.stdout).to.contain('post-1</loc>');
-      expect(ctx.stdout).to.contain('https://example.com/blog/events/event-1</loc>');
-    });
+      expect(ctx.stdout).to.not.contain('.html')
+      expect(ctx.stdout).to.contain('post-1</loc>')
+      expect(ctx.stdout).to.contain('https://example.com/blog/events/event-1</loc>')
+    })
 
   test
     .stdout()
     .do(() => cmd.run(['https://example.com', '--root', 'test/test-site', '--no-clean']))
     .it('no clean urls', (ctx) => {
-      expect(ctx.stdout).to.contain('https://example.com/index.html</loc>');
-      expect(ctx.stdout).to.contain('https://example.com/blog/news/post-2.html</loc>');
-    });
+      expect(ctx.stdout).to.contain('https://example.com/index.html</loc>')
+      expect(ctx.stdout).to.contain('https://example.com/blog/news/post-2.html</loc>')
+    })
 
   test
     .stdout()
     .do(() => cmd.run(['https://example.com', '--root', 'test/test-site', '--slash']))
     .it('trailing slash', (ctx) => {
-      expect(ctx.stdout).to.contain('https://example.com/</loc>');
-      expect(ctx.stdout).to.contain('https://example.com/blog/mixed-1/</loc>');
-    });
+      expect(ctx.stdout).to.contain('https://example.com/</loc>')
+      expect(ctx.stdout).to.contain('https://example.com/blog/mixed-1/</loc>')
+    })
 
   test
     .do(() => cmd.run(['https://example.com', '--root', 'test/test-site/about', '--save']))
     .it('saves to sitemap.xml', () => {
-      let xml = fs.readFileSync('test/test-site/about/sitemap.xml', 'utf-8');
-      expect(xml).to.contain('<loc>https://example.com/</loc>');
-      fs.unlinkSync('test/test-site/about/sitemap.xml');
-      fs.unlinkSync('test/test-site/about/sitemap.txt');
-    });
+      const xml = fs.readFileSync('test/test-site/about/sitemap.xml', 'utf-8')
+      expect(xml).to.contain('<loc>https://example.com/</loc>')
+      fs.unlinkSync('test/test-site/about/sitemap.xml')
+      fs.unlinkSync('test/test-site/about/sitemap.txt')
+    })
 
   test
     .do(() => cmd.run(['https://example.com', '--root', 'test/test-site/about', '--save', '--output-dir', 'test/test-site']))
     .it('saves to sitemap.txt and output-dir works', () => {
-      let txt = fs.readFileSync('test/test-site/sitemap.txt', 'utf-8');
-      expect(txt).to.equal('https://example.com/\n');
-      fs.unlinkSync('test/test-site/sitemap.xml');
-      fs.unlinkSync('test/test-site/sitemap.txt');
-    });
+      const txt = fs.readFileSync('test/test-site/sitemap.txt', 'utf-8')
+      expect(txt).to.equal('https://example.com/\n')
+      fs.unlinkSync('test/test-site/sitemap.xml')
+      fs.unlinkSync('test/test-site/sitemap.txt')
+    })
 
   test
     .stdout()
     .do(() => cmd.run(['https://example.com', '--root', 'test/test-site', '--follow-noindex']))
     .it('ignores files with robots noindex meta tag', (ctx) => {
-      expect(ctx.stdout).to.not.contain('<loc>https://example.com/noindex/not-indexed</loc>');
-      expect(ctx.stdout).to.not.contain('<loc>https://example.com/noindex/not-indexed-2</loc>');
-    });
+      expect(ctx.stdout).to.not.contain('<loc>https://example.com/noindex/not-indexed</loc>')
+      expect(ctx.stdout).to.not.contain('<loc>https://example.com/noindex/not-indexed-2</loc>')
+    })
 
   test
     .stdout()
     .do(() => cmd.run(['https://example.com', '--root', 'test/test-site']))
     .it('does NOT ignore files with robots noindex meta tag when overridden', (ctx) => {
-      expect(ctx.stdout).to.not.contain('<loc>https://example.com/noindex/not-indexed/</loc>');
-      expect(ctx.stdout).to.not.contain('<loc>https://example.com/noindex/not-indexed-2/</loc>');
-    });
+      expect(ctx.stdout).to.not.contain('<loc>https://example.com/noindex/not-indexed/</loc>')
+      expect(ctx.stdout).to.not.contain('<loc>https://example.com/noindex/not-indexed-2/</loc>')
+    })
 
   /*
   test
@@ -118,4 +120,4 @@ describe('#index', () => {
       expect(ctx.stdout)
     });
   */
-});
+})
