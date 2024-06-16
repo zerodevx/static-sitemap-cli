7349f494f53365e1201523551c604fad55cc782f
Jason Lee <jason@zerodevx.com>
2024-06-16 18:58:36 +0800

    Upgrade to `eslint@9`

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import globals from 'globals'
+import js from '@eslint/js'
+import prettier from 'eslint-config-prettier'
+
+/** @type {import('eslint').Linter.FlatConfig} */
+export default [
+  js.configs.recommended,
+  prettier,
+  {
+    languageOptions: {
+      globals: globals.node
+    },
+    rules: {
+      'no-tabs': 'error',
+      'no-unexpected-multiline': 'error'
+    }
+  }
+]
